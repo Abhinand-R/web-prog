a261a0d695e910b9d61a0ee5f529c2391a3fb956
Abhinand R <114525448+Abhinand-R@users.noreply.github.com>
2024-11-08 09:55:29 +0530

    Add files via upload

---
+++ proj/config.php
@@ -0,0 +1,9 @@
+<?php
+    $conn = mysqli_connect("localhost", "root", "", "projdb");
+
+    // if($conn){
+    //     echo "Connected";
+    // }else{
+    //     echo "Error";
+    // }
+?>
---
+++ proj/home.php
@@ -0,0 +1,34 @@
+<?php
+    include 'config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav class="bg-black">
+       <?php
+            include 'header.html'
+        ?> 
+    </nav>
+    
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quod accusamus doloremque asperiores, nulla id sint voluptate, quidem rem qui optio delectus numquam minima placeat est totam maiores omnis dolor.
+    Molestias necessitatibus porro debitis eum quis, veniam ducimus totam exercitationem corrupti ipsa ad inventore neque eveniet aliquid consectetur nulla vitae. Cumque magni similique, laboriosam perspiciatis non libero facere voluptatum repellendus.
+    Nisi accusantium sed minima cupiditate voluptatem obcaecati repudiandae harum cum vel quibusdam necessitatibus reiciendis debitis nemo, delectus nesciunt inventore, ipsam veritatis culpa magni earum nobis repellendus repellat molestiae blanditiis. Sapiente!
+    Ipsa voluptates vero illum! Laboriosam reprehenderit recusandae autem quasi quia magni rerum ipsum fugiat reiciendis ipsa dolorum cumque commodi nisi laborum fugit dolore nesciunt laudantium, hic nulla tempora explicabo optio?
+    Voluptatem debitis ipsam, fugiat sed obcaecati, harum porro similique, quisquam sint perferendis fuga corrupti ad? Commodi officiis possimus consectetur deleniti similique nam tenetur aut excepturi? Commodi eveniet repellendus debitis vero!
+
+
+    <footer class="bg-black">
+        <?php
+            include 'footer.html'
+        ?>
+    </footer>
+</body>
+</html>
